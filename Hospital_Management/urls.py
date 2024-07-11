1a30dee75d84b0b91180ebe8f66e4e2ef14bcc87
unknown <ijsathi2001rs@gmail.com>
2024-07-11 13:31:25 +0600

    service viewset

--- Hospital_Management/urls.py
+++ Hospital_Management/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("contact.urls")),
+    path('', include("service.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
